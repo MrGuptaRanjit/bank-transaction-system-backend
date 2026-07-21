9e5f8b5ce53a06b85945a94da28a924ae20c891d
MrGuptaRanjit <ranjitkumar22129cse@sdbc.ac.in>
2026-07-21 14:13:56 +0530

    Use Render PORT environment variable

--- server.js
+++ server.js
@@ -9,6 +9,12 @@ const connectDB = require("./src/config/db")
 
 connectDB()
 
-app.listen(3000,()=>{
-    console.log("Server is running at PORT : 3000")
-})
+// app.listen(3000,()=>{
+//     console.log("Server is running at PORT : 3000")
+// })
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running at PORT : ${PORT}`);
+});
